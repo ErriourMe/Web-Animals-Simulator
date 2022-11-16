62e9f87a3b4f699091f808a1f24591faa611e88b
ErriourMe <erriour.me@gmail.com>
2022-11-16 17:31:20 +0300

    feat: colors in unocss

--- Client/src/config/unoCSS/colors.ts
+++ Client/src/config/unoCSS/colors.ts
@@ -1,5 +1,8 @@
 interface IColors {
   primary: string | Record<string | number, string>;
+  lightblue: string | Record<string | number, string>;
+  blue: string | Record<string | number, string>;
+  brown: string | Record<string | number, string>;
 }
 
 export const colors: IColors = {
@@ -15,5 +18,43 @@ export const colors: IColors = {
     "700": "#00c268",
     "800": "#00b85e",
     "900": "#00ae54",
+  },
+  lightblue: {
+    DEFAULT: "#e0f7fa",
+    "50": "#ffffff",
+    "100": "#ffffff",
+    "200": "#feffff",
+    "300": "#f4ffff",
+    "400": "#eaffff",
+    "500": "#e0f7fa",
+    "600": "#d6edf0",
+    "700": "#cce3e6",
+    "800": "#c2d9dc",
+    "900": "#b8cfd2",
+  },
+  blue: {
+    DEFAULT: "#80deea",
+    "50": "#b2ffff",
+    "100": "#a8ffff",
+    "200": "#9efcff",
+    "300": "#94f2fe",
+    "400": "#8ae8f4",
+    "500": "#80deea",
+    "600": "#76d4e0",
+    "700": "#6ccad6",
+    "800": "#62c0cc",
+    "900": "#58b6c2",
+  },
+  brown: {
+    "50": "#d7ccc8",
+    "100": "#664f4b",
+    "200": "#5c4541",
+    "300": "#523b37",
+    "400": "#48312d",
+    "500": "#3e2723",
+    "600": "#341d19",
+    "700": "#2a130f",
+    "800": "#200905",
+    "900": "#160000",
   },
 };
