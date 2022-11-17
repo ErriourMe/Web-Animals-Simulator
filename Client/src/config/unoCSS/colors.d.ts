0344f1aee67515c1347225bfd6262a82e9f1edbf
ErriourMe <erriour.me@gmail.com>
2022-11-18 01:50:28 +0300

    chore: fix d.ts for build

---
+++ Client/src/config/unoCSS/colors.d.ts
@@ -0,0 +1,8 @@
+interface IColors {
+  primary: string | Record<string | number, string>;
+  lightblue: string | Record<string | number, string>;
+  blue: string | Record<string | number, string>;
+  brown: string | Record<string | number, string>;
+}
+export declare const colors: IColors;
+export {};
